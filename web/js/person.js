7439e189b75ec00608daf9de1743afe61c0fa8b3
Frederic Ye <yefrederic@gmail.com>
2013-06-29 11:58:41 +0200

    person and show js

---
+++ web/js/person.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {
+
+  window.Person = Backbone.Model.extend({
+    urlRoot: "/api/person",
+    defaults: {
+      "id" : null,
+      "fname" : "",
+      "lname" : "",
+    }
+  });
+
+  window.PersonCollection = Backbone.Collection.extend({
+    model: Person,
+    url: "/api/person",
+    parse: function(response) {
+      return response.persons;
+    }
+  });
+
+  window.PersonListView = Backbone.View.extend({
+
+    tagName:'ul',
+
+    initialize:function () {
+      this.model.bind("reset", this.render, this);
+      var self = this;
+      this.model.bind("add", function (person) {
+        $(self.el).append(new PersonListItemView({model:person}).render().el);
+      });
+    },
+
+    render:function (eventName) {
+      _.each(this.model.models, function (person) {
+        $(this.el).append(new PersonListItemView({model:person}).render().el);
+      }, this);
+      return this;
+    }
+
+  });
+
+  window.PersonListItemView = Backbone.View.extend({
+
+    tagName:"li",
+
+    template:_.template($('#person-list-item').html()),
+
+    initialize:function () {
+      this.model.bind("change", this.render, this);
+      this.model.bind("destroy", this.close, this);
+    },
+
+    events:{
+      "click .delete":"deletePerson"
+    },
+
+    render:function (eventName) {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    close:function () {
+      $(this.el).unbind();
+      $(this.el).remove();
+    },
+
+    deletePerson:function () {
+      this.model.destroy({
+        success:function () {
+          alert('Person deleted successfully');
+          window.history.back();
+        }
+      });
+      return false;
+    },
+
+  });
+
+  window.PersonView = Backbone.View.extend({
+
+    template:_.template($('#person-details').html()),
+
+    initialize:function () {
+      this.model.bind("change", this.render, this);
+    },
+
+    render:function (eventName) {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    events: {
+      "change input":"change",
+      "click .save":"savePerson",
+    },
+
+    savePerson:function () {
+      this.model.set({
+        fname:$('#fname').val(),
+        lname:$('#lname').val(),
+      });
+      if (this.model.isNew()) {
+        app.personList.create(this.model);
+      } else {
+        this.model.save();
+      }
+      return false;
+    },
+
+    close:function () {
+      $(this.el).unbind();
+      $(this.el).empty();
+    }
+
+  });
+
+  window.HeaderView = Backbone.View.extend({
+
+    template: _.template($('#header').html()),
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function(eventName) {
+      $(this.el).html(this.template());
+      return this;
+    },
+
+    events: {
+      "click .new" : "newPerson",
+    },
+
+    newPerson: function(event) {
+      if (app.personView) app.personView.close();
+      app.personView = new PersonView({model:new Person()});
+      $('#person').html(app.personView.render().el);
+      return false;
+    },
+
+  });
+
+    // Router
+  var AppRouter = Backbone.Router.extend({
+
+    routes: {
+      "" : "list",
+      "id=:id" : "personDetails",
+    },
+
+    initialize:function () {
+      $('#add').html(new HeaderView().render().el);
+    },
+
+    list:function () {
+      this.personList = new PersonCollection();
+      this.personListView = new PersonListView({model:this.personList});
+      this.personList.fetch();
+      $('#persons').html(this.personListView.render().el);
+    },
+
+    personDetails:function (id) {
+      this.person = this.personList.get(id);
+      if (app.personView) app.personView.close();
+      this.personView = new PersonView({model:this.person});
+      $('#person').html(this.personView.render().el);
+    }
+
+  });
+
+  var app = new AppRouter();
+  Backbone.history.start();
+
+});
---
+++ web/js/show.js
@@ -0,0 +1,189 @@
+$(document).ready(function() {
+
+  window.Show = Backbone.Model.extend({
+    urlRoot: "/api/show",
+    defaults: {
+      "id" : null,
+      "name" : "",
+      "creators" : "",
+      "cast": "",
+      "genre_id": null,
+      "image": "",
+      "storyline": "",
+      "created_at": null,
+      "updated_at": null,
+    }
+  });
+
+  window.ShowCollection = Backbone.Collection.extend({
+    model: Show,
+    url: "/api/show",
+    parse: function(response) {
+      return response.shows;
+    }
+  });
+
+  window.ShowListView = Backbone.View.extend({
+
+    tagName:'ul',
+    className:'unstyled',
+
+    initialize:function () {
+      this.model.bind("reset", this.render, this);
+      var self = this;
+      this.model.bind("add", function (show) {
+        $(self.el).append(new ShowListItemView({model:show}).render().el);
+      });
+    },
+
+    render:function (eventName) {
+      _.each(this.model.models, function (show) {
+        $(this.el).append(new ShowListItemView({model:show}).render().el);
+      }, this);
+      return this;
+    },
+
+  });
+
+  window.ShowListItemView = Backbone.View.extend({
+
+    tagName:"li",
+
+    template:_.template($('#show-list-item').html()),
+
+    initialize:function () {
+      this.model.bind("change", this.render, this);
+      this.model.bind("destroy", this.close, this);
+    },
+
+    events: {
+      "click .delete" : "deleteShow",
+    },
+
+    render:function (eventName) {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    close:function () {
+      $(this.el).unbind();
+      $(this.el).remove();
+    },
+
+    deleteShow:function () {
+      this.model.destroy({
+        success:function () {
+          alert('Show deleted successfully');
+          window.history.back();
+        }
+      });
+      return false;
+    },
+
+  });
+
+  window.ShowView = Backbone.View.extend({
+
+    template:_.template($('#show-details').html()),
+
+    events: {
+      "click .save" : "saveShow",
+    },
+
+    initialize:function () {
+      this.model.bind("change", this.render, this);
+    },
+
+    render:function (eventName) {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    close:function () {
+      $(this.el).unbind();
+      $(this.el).empty();
+    },
+
+    saveShow:function () {
+      this.model.set({
+        name:$('#name').val(),
+        creators:$('#creators').val(),
+        cast:$('#cast').val(),
+        genre_id:$('#genre_id').val(),
+        storyline:$('#storyline').val(),
+      });
+      if (this.model.isNew()) {
+        app.showList.create(this.model);
+      } else {
+        this.model.save();
+      }
+      $('#show').modal('hide');
+      return false;
+    },
+
+  });
+
+  window.HeaderView = Backbone.View.extend({
+
+    template: _.template($('#header').html()),
+
+    initialize: function() {
+      this.render();
+      $('#show').on('hide', function () {
+        window.location.hash = "";
+      })
+    },
+
+    render: function(eventName) {
+      $(this.el).html(this.template());
+      return this;
+    },
+
+    events: {
+      "click .new" : "newShow",
+    },
+
+    newShow: function(event) {
+      if (app.showView) app.showView.close();
+      app.showView = new ShowView({model:new Show()});
+      $('#show').html(app.showView.render().el);
+      $('#newShowLabel').text('New Show');
+      return true;
+    },
+
+  });
+
+  // Router
+  var AppRouter = Backbone.Router.extend({
+
+    routes: {
+      "" : "list",
+      "edit=:id" : "editDetails",
+    },
+
+    initialize:function () {
+      $('#add').html(new HeaderView().render().el);
+    },
+
+    list:function () {
+      this.showList = new ShowCollection();
+      this.showListView = new ShowListView({model:this.showList});
+      this.showList.fetch();
+      $('#shows').html(this.showListView.render().el);
+    },
+
+    editDetails:function (id) {
+      this.show = this.showList.get(id);
+      if (app.showView) app.showView.close();
+      this.showView = new ShowView({model:this.show});
+      $('#show').html(this.showView.render().el);
+      $('#showLabel').text('Edit Show');
+      $('#show').modal('show');
+    }
+
+  });
+
+  var app = new AppRouter();
+  Backbone.history.start();
+
+});
